67f7f8b47600356865dacf9c2afbe0d3c8303ff0
Felippe Kulkamp Sant ana <123035151+Felippeks@users.noreply.github.com>
2024-03-16 18:03:02 -0300

    adicionado função de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
   {
@@ -9,7 +10,7 @@ export const routes: Routes = [
   },
   {
     path: '',
-    component: LoginComponent,
+    component: HomeComponent,
   },
   {
     path: 'cadastro',
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
+
+  constructor(private router: Router) {}
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      const email = this.loginForm.get('email')?.value;
+      const password = this.loginForm.get('password')?.value;
+
+      if (email && localStorage.getItem(email) === password) {
+        this.router.navigate(['']);
+      } else {
+        alert('Usuário ou senha inválidos');
+      }
+    } else {
+      alert('Por favor, preencha todos os campos corretamente!');
+    }
+  }
+
+  onForgotPassword() {
+    const email = this.loginForm.get('email')?.value;
+
+    if (email && localStorage.getItem(email)) {
+      localStorage.setItem(email, 'a1b2c4d4');
+      alert(
+        'Sua senha foi alterada para a senha padrão: a1b2c4d4. Por favor, prossiga utilizando essa senha.',
+      );
+    } else {
+      alert('Email não encontrado');
+    }
+  }
+
+  onRegister() {
+    this.router.navigate(['/cadastro']);
+  }
+}
