b529f39107490fba7e077c73cb2b964618555723
Felippe Kulkamp Sant ana <123035151+Felippeks@users.noreply.github.com>
2024-03-16 18:31:25 -0300

    adicionado componente header

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -6,7 +6,7 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router } from '@angular/router'; 
 
 @Component({
   selector: 'app-cadastro',
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private router: Router) {}
+
+  goLogin() {
+    this.router.navigate(['/login']);
+  }
+
+  goRegister() {
+    this.router.navigate(['/cadastro']);
+  }
+}
