1b5d48b34e2e3cf987afc46ed0c7a74112d1d54b
Felippe Kulkamp Sant ana <123035151+Felippeks@users.noreply.github.com>
2024-03-16 17:27:36 -0300

    cadastro criado e adicionado as funções

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 export const routes: Routes = [
   {
@@ -9,5 +10,9 @@ export const routes: Routes = [
   {
     path: '',
     component: LoginComponent,
+  },
+  {
+    path: 'cadastro',
+    component: CadastroComponent,
   },
 ];
