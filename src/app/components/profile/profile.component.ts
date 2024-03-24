d3daac4e7a73e5d6c0484f5952a96b3fdd489010
Felippe Kulkamp Sant ana <123035151+Felippeks@users.noreply.github.com>
2024-03-23 23:28:37 -0300

    M1S10 concluido

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { AgePipe } from '../../pipes/age.pipe';
 import { HeightPipe } from '../../pipes/height.pipe';
+import { CepService } from '../../services/address.service';
 
 @Component({
   selector: 'app-profile',
@@ -12,9 +13,20 @@ import { HeightPipe } from '../../pipes/height.pipe';
 })
 export class ProfileComponent implements OnInit {
   userData: any;
-  constructor() {}
+  userAddress: string | undefined; 
+
+  constructor(private cepService: CepService) {} // Injete o serviço
 
   ngOnInit(): void {
     this.userData = JSON.parse(localStorage.getItem('userData') || '{}');
+    this.getAddress(this.userData.location); 
+  }
+
+  getAddress(cep: string) {
+    this.cepService.get(cep).subscribe((data: any) => {
+      if (!data.erro) {
+        this.userAddress = `${data.logradouro}, ${data.complemento} - ${data.bairro} - ${data.localidade}/${data.uf}`;
+      }
+    });
   }
 }
