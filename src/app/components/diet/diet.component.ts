e281064e26ef739a72e5d53ccaf826481c992f6a
Felippe Kulkamp Sant ana <123035151+Felippeks@users.noreply.github.com>
2024-03-26 10:38:49 -0300

    remoçao slider, alteração versao card

--- src/app/components/diet/diet.component.ts
+++ src/app/components/diet/diet.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+// diet.component.ts
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import './diets.mock';
 import { CommonModule } from '@angular/common';
@@ -28,19 +29,12 @@ export interface Diets {
     RouterModule,
   ],
 })
-export class DietComponent implements OnInit, OnDestroy {
+export class DietComponent implements OnInit {
   listLoad: Diets[] = [];
   list: Diets[] = [];
   inputPesquisa = new FormControl('');
 
-  current = 0;
-  timerId: any;
-
   ngOnInit() {
-    this.timerId = setInterval(() => {
-      this.next();
-    }, 5000);
-
     let dietsData = localStorage.getItem('diets');
     if (dietsData) {
       this.listLoad = JSON.parse(dietsData) as Diets[];
@@ -54,26 +48,6 @@ export class DietComponent implements OnInit, OnDestroy {
     });
   }
 
-  ngOnDestroy() {
-    if (this.timerId) {
-      clearInterval(this.timerId);
-    }
-  }
-
-  previous() {
-    if (this.current > 0) {
-      this.current--;
-    }
-  }
-
-  next() {
-    if (this.current < this.list.length - 1) {
-      this.current++;
-    } else {
-      this.current = 0;
-    }
-  }
-
   pesquisa(val: string) {
     if (val.length > 0) {
       this.list = this.listLoad.filter((x) =>
@@ -84,10 +58,6 @@ export class DietComponent implements OnInit, OnDestroy {
     }
   }
 
-  goToSlide(index: number) {
-    this.current = index;
-  }
-  
   constructor(private router: Router) {}
   goToDetails(id: number) {
     this.router.navigate(['/diet', id]);
